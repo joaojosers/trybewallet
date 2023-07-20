14e7f6e83ada6d62079e7af45238ceb0fd3c01a4
Joao Jose Rocha de Sousa <joaojosers@hotmail.com>
2023-07-20 17:17:13 -0300

    requisito 4

--- src/tests/helpers/App.test.tsx
+++ src/tests/helpers/App.test.tsx
@@ -1,12 +1,25 @@
 import { screen } from '@testing-library/dom';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
+import { vi } from 'vitest';
 import App from '../../App';
 import { renderWithRouterAndRedux } from './renderWith';
 import { EMAIL_DATA, PASSWORD_DATA } from '../../redux/actions';
 import Header from '../../components/Header';
+import Wallet from '../../redux/reducers/wallet';
+import WalletForm from '../../components/WalletForm';
+import mockData from './mockData';
 
-describe('App', () => {
+describe('testando a aplicação', () => {
+  beforeEach(() => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: async () => (mockData),
+    });
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
   test('verifica se a tela de login funciona corretamente', async () => {
     renderWithRouterAndRedux(
       <App />,
@@ -32,43 +45,59 @@ describe('App', () => {
   test('verifica as fucionalidades  na página Login', async () => {
     renderWithRouterAndRedux(
       <App />,
-    //   <Header />,
     );
     const inputEmail = screen.getByRole('textbox');
     const inputPassword = screen.getByPlaceholderText(/senha/i);
     const button = screen.getByRole('button', { name: /entrar/i });
-    await userEvent.type(inputEmail, EMAIL_DATA);
-    await userEvent.type(inputPassword, PASSWORD_DATA);
+    await userEvent.type(inputEmail, 'alguem@alguem.com');
+    await userEvent.type(inputPassword, '123456');
     await userEvent.click(button);
-    expect(global.fetch).toHaveBeenCalled();
+    expect(button).toBeEnabled();
+    // expect(global.fetch).toHaveBeenCalled();
     expect(await screen.findByTestId('total-field')).toBeInTheDocument();
     expect(await screen.findByTestId('header-currency-field')).toBeInTheDocument();
     expect(await screen.findByTestId('email-field')).toBeInTheDocument();
   });
-  test('verifica as fucionalidades da página Wallet', async () => {
+  test('verifica as funcionalidades da página Wallet', async () => {
     renderWithRouterAndRedux(
       <App />,
     );
+    const inputEmail = screen.getByRole('textbox');
+    const inputPassword = screen.getByPlaceholderText(/senha/i);
+    const button = screen.getByRole('button', { name: /entrar/i });
+    await userEvent.type(inputEmail, 'ninguem@ninguem.com');
+    await userEvent.type(inputPassword, '654321');
+    await userEvent.click(button);
+
     const title = screen.getByRole('heading', { level: 2, name: /despesas/i });
     expect(title).toBeInTheDocument();
-    // const inputEmail = screen.getByRole('textbox');
-    // const inputPassword = screen.getByPlaceholderText(/senha/i);
-    // const button = screen.getByRole('button', { name: /entrar/i });
-    // await userEvent.type(inputEmail, EMAIL_DATA);
-    // await userEvent.type(inputPassword, PASSWORD_DATA);
-    // await userEvent.click(button);
-    // expect(global.fetch).toHaveBeenCalled();
-    // const inputDescripition = screen.getByRole('textbox', { name: /descrição da despesa/i });
-    // const inputCategory = screen.getByRole('combox', { name: /categoria da despesa/i });
-    // const inputValue = screen.getByRole('spinbutton', { name: /valor/i });
-    // const inputMethod = screen.getByRole('combox', { name: /metodo de pagamento/i });
-    // const inputMoney = screen.getByRole('combox', { name: /moeda/i });
-    // const btn = screen.getByRole('button', { name: /adicionar despesa/i });
-    // expect(inputDescripition).toBeInTheDocument();
-    // expect(inputCategory).toBeInTheDocument();
-    // expect(inputValue).toBeInTheDocument();
-    // expect(inputMethod).toBeInTheDocument();
-    // expect(inputMoney).toBeInTheDocument();
-    // expect(btn).toBeInTheDocument();
+    const inputDescription = screen.getByRole('textbox', { name: /description:/i });
+    expect(inputDescription).toBeInTheDocument();
+    const inputValue = screen.getByRole('spinbutton', { name: /value:/i });
+    expect(inputValue).toBeInTheDocument();
+    const currency = screen.getByRole('combobox', { name: /currency:/i });
+    expect(currency).toBeInTheDocument();
+    const method = screen.getByRole('combobox', { name: /payment method/i });
+    expect(method).toBeInTheDocument();
   });
 });
+
+// describe('Testes que precisam de mock', () => {
+//   beforeEach(() => {
+//     vi.spyOn(global, 'fetch').mockResolvedValue({
+//       json: async () => (valorRetornadoPelaAPI),
+//     });
+//   });
+
+//   afterEach(() => {
+//     vi.clearAllMocks();
+//   });
+
+//   test('Este teste já possui o mock', async () => {
+//     // ...
+//   });
+
+//   test('Este teste também já possui o mock', async () => {
+//     // ...
+//   });
+// });
